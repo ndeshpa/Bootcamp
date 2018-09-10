febb2cc746068c0c03a2e5e6633d2f686ebf1255
Nita Deshpande <nita.deshpande@emory.edu>
2018-09-10 10:38:51 -0400

    Added csv file location as argument

--- MedDataPlotGrouping.py
+++ MedDataPlotGrouping.py
@@ -4,28 +4,30 @@ Created on Wed Sep  5 11:28:40 2018
 
 @author: Nita
 """
-
+import sys
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-plt.rcParams['figure.figsize']=(20,10) # set the figure size
-plt.style.use('fivethirtyeight') # using the fivethirtyeight matplotlib theme
-#Set up Fonts for the text on the plot
-font = {'family' : 'normal',
+if(len(sys.argv) ==1):
+    print("Please enter the complete path of the csv file. Ex: C:/your/folder/name/filename.csv")
+    sys.exit
+else:
+    #Set up Fonts for the text on the plot
+    font = {'family' : 'normal',
         'weight' : 'bold',
         'size'   : 24}
-plt.rc('font', **font)
+    plt.rc('font', **font)
 
-medData=pd.read_csv("C:/Users/Nita/Documents/PythonScripts/processed.cleveland.data.csv")
-medDataDf = pd.DataFrame(medData)
-print(medDataDf)
+    medData=pd.read_csv(sys.argv[1])
+    medDataDf = pd.DataFrame(medData)
+    print(medDataDf)
 
-#Group people into Age-groups and print out counts for each group and plot a bar graph
-bins =  np.arange(20,90,10)
-binLabels = ['21-30','31-40', '41-50', '51-60', '61-70', '71-80']
-binData=medDataDf.groupby(pd.cut(medDataDf['Age'], bins=bins, labels=binLabels)).size().reset_index(name='patient_count')
-barPlot = binData.plot.bar(rot=0, color="b", figsize=(12,8))
-barPlot.set_xticklabels(binLabels)
-barPlot.set_ylabel('Number of Patients')
+    #Group people into Age-groups and print out counts for each group and plot a bar graph
+    bins =  np.arange(20,90,10)
+    binLabels = ['21-30','31-40', '41-50', '51-60', '61-70', '71-80']
+    binData=medDataDf.groupby(pd.cut(medDataDf['Age'], bins=bins, labels=binLabels)).size().reset_index(name='patient_count')
+    barPlot = binData.plot.bar(rot=0, color="b", figsize=(12,8))
+    barPlot.set_xticklabels(binLabels)
+    barPlot.set_ylabel('Number of Patients', labelpad=15)
 
--- OverlayPlot.py
+++ OverlayPlot.py
@@ -4,34 +4,42 @@ Created on Thu Sep  6 06:52:55 2018
 
 @author: Nita
 """
-
+import sys
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib import style
- 
-plt.rcParams['figure.figsize']=(20,10) # set the figure size
-plt.style.use('fivethirtyeight') # using the fivethirtyeight matplotlib theme
-#Set up Fonts for the text on the plot
-font = {'family' : 'normal',
+
+if(len(sys.argv) == 1):
+    print("Please enter the complete path of the csv file. Ex: C:/your/folder/name/filename.csv")
+    sys.exit
+else:
+    # set the figure size
+    plt.rcParams['figure.figsize']=(20,10) 
+    # using the fivethirtyeight matplotlib theme
+    #Set up Fonts for the text on the plot
+    plt.style.use('fivethirtyeight')
+    font = {'family' : 'normal',
         'weight' : 'bold',
         'size'   : 24}
-plt.rc('font', **font)
- 
-seqs = pd.read_csv('C:/Users/Nita/Documents/PythonScripts/GenbankStats.csv') # Read the data in
-print(seqs.loc[:,['Date']])
-seqs.Date = pd.to_datetime(seqs.Date, infer_datetime_format=True) #set the date column to datetime
-print(seqs.loc[:,['Date']])
-seqs.set_index('Date', inplace=True) #set the index to the date column
-print(seqs.index)
-#group by year and sum
-summed_data=seqs['Sequences'].resample('A').sum()
-print(summed_data)
+    plt.rc('font', **font)
+    
+     # Read the data in
+    seqs = pd.read_csv(sys.argv[1])
+    print(seqs.loc[:,['Date']])
+    #set the date column to datetime
+    seqs.Date = pd.to_datetime(seqs.Date, infer_datetime_format=True)
+    print(seqs.loc[:,['Date']])
+    #set the index to the date column
+    seqs.set_index('Date', inplace=True)
+    print(seqs.index)
+    #group by year and sum
+    summed_data=seqs['Sequences'].resample('A').sum()
+    print(summed_data)
 
-#fig = plt.figure()
-ax1 = plt.subplot2grid((1,1), (0,0))
-ax1.set_title('Genbank Annual Sequence Submissions')
-ax1.get_yaxis().get_major_formatter().set_scientific(False)
-summed_data.plot(color='r')
-plt.legend()
-plt.show()
+    ax1 = plt.subplot2grid((1,1), (0,0))
+    ax1.set_title('Genbank Annual Sequence Submissions')
+    ax1.get_yaxis().get_major_formatter().set_scientific(False)
+    summed_data.plot(color='r')
+    plt.legend()
+    plt.show()
